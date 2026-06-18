6dfec422e6f77a4e0bc50d6a3b927aafeb4461e9
mildajocaite <132597658+mildajocaite@users.noreply.github.com>
2026-06-18 12:56:54 +0300

    Fix typo

--- app/data/exploding-kittens.ts
+++ app/data/exploding-kittens.ts
@@ -3,7 +3,7 @@ import {Game} from './games';
 export const explodingKittens: Game = {
     id: "exploding-kitten",
     logo: "/games/exploding-kittens.svg",
-    name: "Exploiding Kittens",
+    name: "Exploding Kittens",
     description: "Avoid drawing an Exploding Kitten card. If you draw one, you explode and are out of the game unless you have a Defuse card to defuse it.",
     minPlayers: 2,
     maxPlayers: 8,
